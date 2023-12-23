1489410a7ce8d97feca13967d99da923b25bce92
Ethan <ethantam@berkeley.edu>
2023-12-22 18:12:02 -0800

    updates

---
+++ guess/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Textbox from './components/Textbox.js'
+
+function App() {
+  return (
+    <>
+      <h1>Guess the Number</h1>
+      <Textbox></Textbox>
+    </>
+    
+
+  );
+}
+
+export default App;
