a103696f05a7d7f74b20fb018543570f1d527aee
Ethan <ethantam@berkeley.edu>
2024-01-12 14:10:28 -0800

    removed empty files

--- main/src/components/test.js
+++
@@ -1,6 +0,0 @@
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-require('dotenv').config()
-const key = process.env.REACT_APP_OPENAI_API_KEY;
-
-console.log(key);
