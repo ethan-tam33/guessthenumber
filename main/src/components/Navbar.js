95f2750387ed3f5e08d76bd781311de2a91e2ff0
Ethan <ethantam@berkeley.edu>
2024-01-13 23:18:01 -0800

    conditional profile appearance

--- main/src/components/Navbar.js
+++ main/src/components/Navbar.js
@@ -1,12 +1,37 @@
 import './Navbar.css'
+import {auth} from '../firebase.js'
 
 function Navbar() {
+    function showProfileTab() {
+        document.getElementById('profileTab').style.display = 'block';
+    }
+      
+    function hideProfileTab() {
+        document.getElementById('profileTab').style.display = 'none';
+    }
+
+    auth.onAuthStateChanged(user => {
+        if (user) {
+          // User is signed in, show the "Profile" tab
+          showProfileTab();
+        } else {
+          // User is signed out, hide the "Profile" tab
+          hideProfileTab();
+        }
+      });
+
     return (
         <>
             <nav className="nav">
                 <a href="/" className="site-title">Guess the Number</a>
 
                 <ul>
+                    <li>
+                        <a id='profileTab' href='profile'>Profile</a>
+                    </li>
+                    <li>
+                        <a href='leaderboard'>Leaderboard</a>
+                    </li>
                     <li>
                         <a href="/login">Login</a>
                     </li>
